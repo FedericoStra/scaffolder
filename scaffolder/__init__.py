8b1c6c3b4b26a150d17287a5b83cac2c22fccd15
Federico Stra <stra.federico@gmail.com>
2017-11-07 23:57:28 +0100

    add argument parser (initial setup)

--- scaffolder/__init__.py
+++ scaffolder/__init__.py
@@ -1,11 +1,25 @@
 from configparser import ConfigParser, ExtendedInterpolation
-import argparse
+from argparse import ArgumentParser
 import os
 
 from .template import load_template_yaml
 
 
 def main():
+    parser = ArgumentParser()
+    parser.add_argument('-i', '--interactive', action='store_true')
+    parser.add_argument('-v', '--verbose', action='count', default=0)
+    subparsers = parser.add_subparsers(title='commands', description='valid commands to execute', help='help me', dest='command')
+    sub_create = subparsers.add_parser('create')
+    sub_create.add_argument('-t', help='name of the template to use', metavar='<template>')
+    sub_create.add_argument('-T', help='load the template from %(metavar)s', metavar='<file>')
+    sub_create.add_argument('-d', help='where to create the scaffolding (DEFAULT: %(default)s)', metavar='<dir>', default=os.curdir)
+    sub_create.add_argument('-z', default=42)
+    subparsers.add_parser('list', help='help me listing!')
+    namespace = parser.parse_args()
+
+    print(namespace)
+
     config = ConfigParser(interpolation=ExtendedInterpolation())
     config.read("scaffolder.cfg")
 
