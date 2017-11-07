c5ff36c7123e778b1a5d96837a155ac3d3bb3e3f
Federico Stra <stra.federico@gmail.com>
2017-11-07 23:14:13 +0100

    add loading of configuration

--- scaffolder/__init__.py
+++ scaffolder/__init__.py
@@ -1,9 +1,17 @@
-import yaml
+from configparser import ConfigParser, ExtendedInterpolation
+import argparse
 import os
 
 from .template import load_template_yaml
 
+
 def main():
+    config = ConfigParser(interpolation=ExtendedInterpolation())
+    config.read("scaffolder.cfg")
+
+    template_name = config['DEFAULT']['template']
+    template_file = config[template_name]['source']
+
     tree = load_template_yaml("templates/default.yaml")
     tree.absolute(os.curdir)
     tree.mktree(year=2017)
